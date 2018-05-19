0ec80f1a16977f6073647152cf28fc93dd9a74eb
DiggeHummel <smack11@freenet.de>
2018-05-19 18:25:37 +0200

    little structure improvement

--- VirtualMachine_JavaFX/src/gui/FileManager.java
+++ VirtualMachine_JavaFX/src/gui/FileManager.java
@@ -16,19 +16,22 @@ public class FileManager {
 	private static final String DEFAULT = "ChooseFile";
 
 	/* Constructor */
-	public FileManager() {}
+	public FileManager() {
+	}
 
 	public File chooseFile() {
 		FileChooser chooser = new FileChooser();
 		chooser.setTitle(FileManager.DEFAULT);
-    	chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter(".vm", ".vm"));
-    	if(lastDirectory != null) chooser.setInitialDirectory(lastDirectory);
-    	File selFile = chooser.showOpenDialog(new Stage());
-    	if(selFile != null) {
-    		lastDirectory = selFile.getParentFile();
-    		fileName = StringModifier.substring(selFile.getName(), 0, ".");
-    		return selFile;
-    	}else return null;		
+		chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter(".vm", ".vm"));
+		if (lastDirectory != null)
+			chooser.setInitialDirectory(lastDirectory);
+		File selFile = chooser.showOpenDialog(new Stage());
+		if (selFile != null) {
+			lastDirectory = selFile.getParentFile();
+			fileName = StringModifier.substring(selFile.getName(), 0, ".");
+			return selFile;
+		} else
+			return null;
 	}
 
 	public String getFileName() {
