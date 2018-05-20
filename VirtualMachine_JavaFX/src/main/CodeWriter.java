c48346997c21f0d8dccb658a93860ae6a6114a3f
DiggeHummel <smack11@freenet.de>
2018-05-20 12:59:42 +0200

    new Structure for CodeWriter. CodeWriter now only set Flags for new Class Translator. Class Translator translates vm code in asm. The Flags helps to get more efficient.

--- VirtualMachine_JavaFX/src/main/CodeWriter.java
+++ VirtualMachine_JavaFX/src/main/CodeWriter.java
@@ -6,19 +6,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import application.ApplicationController;
+import util.Translator;
 
 public class CodeWriter {
-
-	/* fields */
-	private final String ARITHMETIC_A = "@SP\n" + "AM=M-1\n" + "D=M\n" + "A=A-1\n"; // !SP is reduced by 1
-	private final String DISSOLVE_POINTER = "A=M\n";
 
 	/* VAR */
 	private ApplicationController controller;
 	private BufferedWriter bw;
 	private int JMP_Count = 0;
+	private final Translator translator;
 
 	public CodeWriter(File outputFile) {
+		this.translator = new Translator();
 		try {
 			this.bw = new BufferedWriter(new FileWriter(outputFile));
 		} catch (IOException e) {
@@ -26,145 +25,36 @@ public class CodeWriter {
 		}
 	}
 
-	public void setFileName(String fileName) {
-		// useless
-	}
-
-	private String ARITHMETIC_JMP(String JMP_Case) {
-		return this.ARITHMETIC_A + "D=M-D\n" + "@PUT_TRUE" + JMP_Count + "\n" + "D;" + JMP_Case + "\n" + "@SP\n"
-				+ "A=M-1\n" + "M=0\n" + "@SKIP" + JMP_Count + "\n" + "0;JMP\n" + "(PUT_TRUE" + JMP_Count + ")\n"
-				+ "@SP\n" + "A=M-1\n" + "M=-1\n" + "(SKIP" + JMP_Count + ")\n";
-	}
-
 	public void writeArithmetic(String command) {
-		String code = "";
-		switch (command) {
-		case "add":
-			code = this.ARITHMETIC_A + "M=M+D\n";
-			break;
-		case "sub":
-			code = this.ARITHMETIC_A + "M=M-D\n";
-			break;
-		case "neg":
-			code = "@SP\nA=M-1\nM=-M\n";
-			break;
-		case "eq":
-			code = ARITHMETIC_JMP("JEQ");
-			JMP_Count++;
-			break;
-		case "lt":
-			code = ARITHMETIC_JMP("JLT");
-			JMP_Count++;
-			break;
-		case "gt":
-			code = ARITHMETIC_JMP("JGT");
-			JMP_Count++;
-			break;
-		case "and":
-			code = this.ARITHMETIC_A + "M=M&D\n";
-			break;
-		case "or":
-			code = this.ARITHMETIC_A + "M=M|D\n";
-			break;
-		case "not":
-			code = "@SP\nA=M-1\nM=!M\n";
-			break;
-		default:
-			throw new IllegalArgumentException();
+		String flag = "";
+		if(command.equals("lt") || command.equals("eq") || command.equals("gt")){
+			flag = Translator.JMP_FLAG;
+			this.JMP_Count++;
+		}else {
+			flag = Translator.ARITHMETIC_FLAG;
 		}
-		write(code);
-	}
-
-	private String PUSH(String segment, int index, String dissolvePTR) {
-		return "@" + segment + "\n" + dissolvePTR + "D=A\n" + "@" + index + "\n" + "A=A+D\n" + "D=M\n" + "@SP\n"
-				+ "A=M\n" + "M=D\n" + "@SP\n" + "M=M+1\n";
-	}
-
-	private String POP(String segment, int index, String dissolvePTR) {
-		return "@" + segment + "\n" + dissolvePTR + "D=A\n" + "@" + index + "\n" + "D=A+D\n" + "@R13\n" + "M=D\n"
-				+ "@SP\n" + "A=M-1\n" + "D=M\n" + "@R13\n" + "A=M\n" + "M=D\n" + "@SP\n" + "M=M-1\n";
+		write(translator.getASMCode(flag, command, "", 0, JMP_Count));
 	}
 
 	public void writePushPop(String command, String segment, int index) {
 		if (index < 0)
 			throw new IllegalArgumentException();
-		String code = "";
-		switch (command) {
-		case "push":
-			switch (segment) {
-			case "constant":
-				code = "@" + index + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-				break;
-			case "pointer":
-				switch (index) {
-				case 0:
-					code = PUSH("THIS", 0, "");
-					break;
-				case 1:
-					code = PUSH("THAT", 0, "");
-					break;
-				default:
-					throw new IllegalArgumentException();
-				}
-				break;
-			case "static":
-				code = PUSH("16", index, "");
-				break;
-			case "temp":
-				code = PUSH("R5", index, "");
-				break;
-			case "local":
-				code = PUSH("LCL", index, this.DISSOLVE_POINTER);
-				break;
-			case "argument":
-				code = PUSH("ARG", index, this.DISSOLVE_POINTER);
-				break;
-			case "this":
-				code = PUSH("THIS", index, this.DISSOLVE_POINTER);
-				break;
-			case "that":
-				code = PUSH("THAT", index, this.DISSOLVE_POINTER);
-				break;
+		String flag = "";
+		if (segment.equals("pointer")) {
+			flag = Translator.POINTER_FLAG;
+		} else if (command.equals("push")) {
+			if (segment.equals("constant")) {
+				flag = Translator.CONSTANT_FLAG;
+			} else {
+				flag = Translator.PUSH_FLAG;
 			}
-			break;
-		case "pop":
-			switch (segment) {
-			case "pointer":
-				switch (index) {
-				case 0:
-					code = POP("THIS", 0, "");
-					break;
-				case 1:
-					code = POP("THAT", 0, "");
-					break;
-				default:
-					throw new IllegalArgumentException();
-				}
-				break;
-			case "static":
-				code = POP("16", index, "");
-				break;
-			case "temp":
-				code = POP("R5", index, "");
-				break;
-			case "local":
-				code = POP("LCL", index, this.DISSOLVE_POINTER);
-				break;
-			case "argument":
-				code = POP("ARG", index, this.DISSOLVE_POINTER);
-				break;
-			case "this":
-				code = POP("THIS", index, this.DISSOLVE_POINTER);
-				break;
-			case "that":
-				code = POP("THAT", index, this.DISSOLVE_POINTER);
-				break;
-			}
-			break;
-		default:
+		} else if (command.equals("pop")) {
+
+			flag = Translator.POP_FLAG;
+
+		} else
 			throw new IllegalArgumentException();
-		}
-		write(code);
+		write(translator.getASMCode(flag, command, segment, index, JMP_Count));			
 	}
 
 	public void writeInit() {
@@ -174,27 +64,26 @@ public class CodeWriter {
 	public void writeLabel(String arg) {
 		write("(" + arg + ")\n");
 	}
-	
+
 	public void writeGoto(String arg) {
-		write("@" + arg +"\n0;JMP\n");
+		write("@" + arg + "\n0;JMP\n");
 	}
-	
+
 	public void writeIf(String arg) {
 		write("@SP\nA=M-1\nD=M\n@" + arg + "\nD;JNE\n");
 	}
-	
+
 	public void writeCall(String functionName, int numArgs) {
-		
 	}
-	
+
 	public void writeReturn() {
-		
+
 	}
-	
+
 	public void writeFunction(String functionName, int numLocals) {
-		
+
 	}
-	
+
 	private void write(String asmCode) {
 		try {
 			asmCode = asmCode.replaceAll("\n", "\r\n");
